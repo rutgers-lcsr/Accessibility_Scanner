204263ec5c080aef4eb533b4511a6f6997eb722f
Mohamad Khawam <mk1800@rutgers.edu>
2025-09-15 20:32:20 -0400

    updating database

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+flask db upgrade
+if [ $? -eq 0 ]; then
+    echo "Database migrations completed successfully"
+else
+    echo "Database migrations failed"
+    exit 1
+fi
+
 python -c "
 from app import create_app, init_admin
 
