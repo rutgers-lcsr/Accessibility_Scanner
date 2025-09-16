2c8d6aa17415fc74860e046c271e27a2cf0c1c43
Mohamad Khawam <mk1800@rutgers.edu>
2025-09-16 18:53:22 -0400

    making sure all models are imported

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from config import SQLALCHEMY_DATABASE_URI
+from .user import *
+from .website import *
+from .report import *
+from .rules import *
+from .settings import *
+from .utils import *
 
 if SQLALCHEMY_DATABASE_URI.startswith("sqlite"):
     db = SQLAlchemy()
 else:
     db = SQLAlchemy(metadata=MetaData(schema="a11y"))
+    
