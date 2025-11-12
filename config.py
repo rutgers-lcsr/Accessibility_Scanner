43d658d4369e5903b5963ca12ea1186de44724d4
Mohamad Khawam <mk1800@rutgers.edu>
2025-11-11 19:34:38 -0500

    Added back MAIL_USE_SSL AND MAIL_USE_TLS. Added MAIL settings to worker.

--- config.py
+++ config.py
@@ -23,16 +23,16 @@ FLASK_ENV = os.environ.get("FLASK_ENV", "development")
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "KHJADoishdjfo")
 JWT_TOKEN_LOCATION = ['headers', 'cookies']
 
+SITE_ADMINS = os.environ.get("SITE_ADMINS", "mk1800@rutgers.edu").split(",")
 
 HOSTNAME = os.environ.get("HOSTNAME", "localhost")
 CLIENT_URL = os.environ.get("CLIENT_URL", "http://localhost:3000")
 TESTING = os.environ.get("TESTING", "False") == "True"
 MAIL_SERVER = os.environ.get("MAIL_SERVER", "mx.farside.rutgers.edu")
 MAIL_PORT = os.environ.get("MAIL_PORT", 25)
-SITE_ADMINS = os.environ.get("SITE_ADMINS", "mk1800@rutgers.edu").split(",")
 MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "help@cs.rutgers.edu")
-MAIL_USE_SSL= False
-MAIL_USE_TLS = True
+MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", "False") == "True"
+MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/1")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
